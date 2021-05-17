14bc7ca1af817ef23789641324a3774f263b3c7f
Damir Čohadarević <cohadar@gmail.com>
2021-05-17 11:28:59 +0200

    meh

--- код/aski.py
+++ код/aski.py
@@ -1,5 +1,15 @@
+def ок(број):
+    if број < 0 or број >= 100:
+        return False
+    if број >= 27 and број < 33:
+        return False
+    if број in [34, 39, 96]:
+        return False
+    return True
+
+
 def двоцифрени(ндб):
-    return ''.join(chr(број) for број in ндб)
+    return ''.join(chr(број) for број in ндб if ок(број))
 
 
 def main():
--- тестови/test_aski.py
+++ тестови/test_aski.py
@@ -4,3 +4,19 @@ from aski import двоцифрени
 def test0():
     assert двоцифрени([64, 65, 66]) == '@AB'
 
+
+def test1():
+    assert двоцифрени(list(range(27, 33))) == ''
+
+
+def test2():
+    assert двоцифрени([-3, -2, -1]) == ''
+
+
+def test3():
+    assert двоцифрени([100, 101, 200, 3000]) == ''
+
+
+def test4():
+    assert двоцифрени(list(range(33, 100))) == '!#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_abc'
+
