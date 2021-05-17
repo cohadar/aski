5efb1977eba4fb3917d6ab75adf55c3f6efcedd2
Damir Čohadarević <cohadar@gmail.com>
2021-05-17 11:17:05 +0200

    meh

--- код/aski.py
+++ код/aski.py
@@ -1,3 +1,7 @@
+def двоцифрени(ндб):
+    return ''.join(chr(број) for број in ндб)
+
+
 def main():
     print('aaa')
 
---
+++ тестови/test_aski.py
@@ -0,0 +1,6 @@
+from aski import двоцифрени
+
+
+def test0():
+    assert двоцифрени([64, 65, 66]) == '@AB'
+
